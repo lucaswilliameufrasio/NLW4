1ad60858f2df42e438484d965d460fe307ded085
Lucas William <lucaswilliameufrasio@gmail.com>
2021-02-28 00:09:08 -0300

    test: ensure UsersView renders created user correctly

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,35 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Any Name",
+      password: "any_password",
+      nickname: "any_nickname",
+      email: "any@email.com",
+      age: "21"
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{balance: Decimal.new("0.00"), id: account_id},
+        id: user_id,
+        name: "Any Name",
+        nickname: "any_nickname"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
